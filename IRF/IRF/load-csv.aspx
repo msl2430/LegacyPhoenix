1723709e7859aaf754c117b9dda2d4767b88d199
msl2430 <me@mikeslevine.com>
2014-11-15 09:27:46 -0500

    Initial commit

---
+++ IRF/IRF/load-csv.aspx
@@ -0,0 +1,91 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Site.Master" AutoEventWireup="true" CodeBehind="load-csv.aspx.cs" Inherits="IRF.load_csv" ClientIDMode="Static" %>
+<asp:Content ID="Content1" ContentPlaceHolderID="HeadContent" runat="server">
+    <style type="text/css">
+        #divLeft { float: left; width: 400px; margin-left: 20px; }
+        #divRight { float: left; width: 500px;} 
+        h2 { font-family: Calibri !important; font-size: 12pt !important; }
+        .buttons 
+        {
+        	outline: none;
+        	margin: 0 4px 0 0;
+        	padding: .4em 1em;
+        	text-decoration: none !important;
+        	cursor: pointer; 
+        	position: relative;
+        	text-align: center;
+
+        }    
+        fieldset { width: 300px; font-family: Calibri !important; font-size: 11pt !important; padding: 10px;}
+        legend { font-family: Calibri !important; font-size: 12pt !important; } 
+        #fileCSV { margin-top: 8px; } 
+        #btnUploadFile { margin-top: 10px; }
+        #divError { font-family: Calibri !important; font-size: 10pt !important; width: 250px; margin-top: 5px; }
+        #divError p { margin: 5px; }
+        #divSuccess { font-family: Calibri !important; font-size: 10pt !important; width: 250px; margin-top: 5px; color: Green; }
+        #divSuccess p { margin: 5px; }
+        .PrimRow { background-color: #eaeaea; text-align: center;}
+        .AltRow { background-color: #gbgbgb; text-align: center; } 
+    </style>
+</asp:Content>
+<asp:Content ID="Content2" ContentPlaceHolderID="MainContent" runat="server">
+    <div id="divMain">
+        <div id="divLeft">
+            <fieldset>
+                <legend><strong>File Selection</strong></legend>
+                <label>Please select a CSV file to upload:</label><br />
+                <asp:FileUpload ID="fileCSV" runat="server" /><br />
+                <div id="divSuccess" class="ui-state-highlight ui-corner-all" runat="server" visible="false" style="border-color: Green;">
+                    <table>
+                        <tr>
+                            <td><span class="ui-icon ui-icon-check"></span></td>
+                            <td><p><strong><label id="lblSuccessMsg" runat="server"></label></strong></p></td>
+                        </tr>
+                    </table>
+                </div>
+                <div id="divError" class="ui-state-error ui-corner-all" runat="server" visible="false">
+                    <table>
+                        <tr>
+                            <td><span class="ui-icon ui-icon-alert"></span></td>
+                            <td><p><strong><label id="lblErrorMsg" runat="server"></label></strong></p></td>
+                        </tr>
+                    </table>
+                </div>
+                <asp:Button ID="btnUploadFile" runat="server" Text="Upload" onclick="btnUploadFile_Click" class="buttons ui-state-default ui-corner-all" />
+            </fieldset>            
+            <br /><br />
+            <h2>IRF Cases In Queue (<%= QueuedCases %> <%= (QueuedCases == 1)? "case" : "cases" %>):</h2>
+            <asp:GridView ID="grvQueue" runat="server" Width="325px" Font-Names="Calibri" Font-Size="Medium" AutoGenerateColumns="false">
+                <RowStyle CssClass="PrimRow" />
+                <AlternatingRowStyle CssClass="AltRow" />
+                <Columns>
+                    <asp:BoundField HeaderText="Case Number" DataField="Case Number" />
+                    <asp:BoundField HeaderText="Added" DataField="Added" />
+                </Columns>
+            </asp:GridView>
+        </div>
+        <div id="divRight">
+            <h2>IRF Cases Processed Today (<%= ProcessedCases %> <%= (ProcessedCases == 1)? "case" : "cases" %>):</h2>
+            <asp:GridView ID="grvProcessed" runat="server" Width="450px" Font-Names="Calibri" Font-Size="Medium" AutoGenerateColumns="false">
+                <RowStyle CssClass="PrimRow" />
+                <AlternatingRowStyle CssClass="AltRow" />
+                <Columns>
+                    <asp:TemplateField>
+                        <ItemTemplate><%# Container.DataItemIndex + 1 %></ItemTemplate>
+                    </asp:TemplateField>
+                    <asp:BoundField HeaderText="Case Number" DataField="Case Number" />
+                    <asp:BoundField HeaderText="Time" DataField="Time" />
+                    <asp:BoundField HeaderText="Result" DataField="Result" />                      
+                    <asp:BoundField HeaderText="Reason" DataField="Reason" />
+                </Columns>
+            </asp:GridView>
+        </div>
+    </div>
+    <script type="text/javascript">
+        $(function () {
+            $("input.buttons").hover(
+                function () { $(this).addClass("ui-state-hover"); },
+                function () { $(this).removeClass("ui-state-hover"); }
+            );
+        });
+    </script>
+</asp:Content>
---
+++ IRF/IRF/load-csv.aspx.cs
@@ -0,0 +1,150 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.IO;
+using System.Text.RegularExpressions;
+using Microsoft.Practices.EnterpriseLibrary.Data;
+using System.Data.Common;
+using System.Data;
+
+namespace IRF
+{
+    public partial class load_csv : System.Web.UI.Page
+    {
+        private string TempFileName =  String.Concat(HttpContext.Current.Server.MapPath("CSVFile"), "/temp.csv");
+        
+        public int QueuedCases { get; set; }
+        public int ProcessedCases { get; set; } 
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            grvQueue.DataSource = GetQueue();
+            grvQueue.DataBind();
+            grvProcessed.DataSource = GetProcessed();
+            grvProcessed.DataBind();
+        }
+
+        protected void btnUploadFile_Click(object sender, EventArgs e)
+        {
+            if (fileCSV.HasFile)
+            {
+                Match regexMatch = Regex.Match(fileCSV.FileName, @"\.(csv)$", RegexOptions.IgnoreCase);
+                if (regexMatch.Success)
+                {
+                    fileCSV.SaveAs(TempFileName);
+                    LoadCSV(TempFileName);
+                    File.Delete(TempFileName);
+                }
+                else
+                {
+                    lblErrorMsg.InnerText = "This file format is not supported. Please select a CSV (*.csv) file.";
+                    divError.Visible = true;
+                    divSuccess.Visible = false;
+                }
+            }
+            else
+            {
+                lblErrorMsg.InnerText = "A file must be selected.";
+                divError.Visible = true;
+                divSuccess.Visible = false;
+            }
+            grvQueue.DataSource = GetQueue();
+            grvQueue.DataBind();
+        }
+
+        private void LoadCSV(string Path)
+        {
+            int count = 0;
+            try
+            {  
+                using (StreamReader fStream = new StreamReader(Path))
+                {
+                    while(fStream.Peek() != -1)
+                    {
+                        InsertCaseNumber(fStream.ReadLine());
+                        count++;
+                    }
+                }
+                lblSuccessMsg.InnerText = String.Format("Successfully added {0} {1}.", count.ToString(), (count == 1) ? "case" : "cases");
+                divSuccess.Visible = true;
+                divError.Visible = false;
+
+            }
+            catch (Exception ex)
+            {
+                lblErrorMsg.InnerText = ex.Message;
+                divError.Visible = true;
+                divSuccess.Visible = false;
+            }
+        }
+        
+        private void InsertCaseNumber(string casenumber)
+        {
+            Database db = DatabaseFactory.CreateDatabase();
+            DbCommand cmd = db.GetSqlStringCommand(@"INSERT INTO IRFCaseQueue VALUES (@CaseNumber, @Date, @Time)");
+            db.AddInParameter(cmd, "CaseNumber", DbType.String, casenumber);
+            db.AddInParameter(cmd, "Date", DbType.Date, DateTime.Now.Date.ToString());
+            db.AddInParameter(cmd, "Time", DbType.Time, DateTime.Now.TimeOfDay.ToString());
+            
+            db.ExecuteNonQuery(cmd);
+        }
+
+        private DataTable GetQueue()
+        {
+            Database db = DatabaseFactory.CreateDatabase();
+            DbCommand cmd = db.GetSqlStringCommand(@"SELECT CaseNumber, CAST(CAST([Date] AS VARCHAR(15)) + ' ' + CAST([Time] AS VARCHAR(25)) AS DATETIME) AS 'tempTime' FROM IRFCaseQueue ORDER BY 'tempTime' DESC");
+            DataTable dtResult = new DataTable();
+            dtResult.Columns.Add("Case Number");
+            dtResult.Columns.Add("Added");
+            using (IDataReader sqlReader = db.ExecuteReader(cmd))
+            {
+                while (sqlReader.Read()) 
+                {
+                    object[] dRow = new object[2];
+                    dRow[0] = sqlReader.GetString(0);
+                    TimeSpan diff = DateTime.Now.Subtract(sqlReader.GetDateTime(1));
+                    if (diff.Days > 0) { dRow[1] = String.Format("{0} {1} ago", diff.Days, (diff.Days > 1) ? "days" : "day"); }
+                    else if (diff.Hours > 0) { dRow[1] = String.Format("{0} {1} ago", diff.Hours, (diff.Hours > 1) ? "hours" : "hour"); }
+                    else { dRow[1] = String.Format("{0} {1} ago", diff.Minutes, (diff.Minutes == 0 || diff.Minutes > 1) ? "minutes" : "minute"); }
+                    
+                    var newRow = dtResult.NewRow();
+                    newRow.ItemArray = dRow;
+                    dtResult.Rows.Add(newRow);
+                }
+            }
+            QueuedCases = dtResult.Rows.Count;
+            return dtResult;
+        }
+
+        private DataTable GetProcessed()
+        {
+            Database db = DatabaseFactory.CreateDatabase();
+            DbCommand cmd = db.GetStoredProcCommand("GetProcessedIRF");
+            db.AddInParameter(cmd, "searchDate", DbType.DateTime, DateTime.Now.ToShortDateString());
+            DataTable dtResult = new DataTable();
+            dtResult.Columns.Add("Case Number");
+            dtResult.Columns.Add("Time");
+            dtResult.Columns.Add("Result");
+            dtResult.Columns.Add("Reason");
+            using (IDataReader sqlReader = db.ExecuteReader(cmd))
+            {
+                while (sqlReader.Read())
+                {
+                    object[] dRow = new object[4];
+                    dRow[0] = sqlReader.GetString(0);
+                    dRow[1] = sqlReader.GetString(1).Substring(0, 5);
+                    dRow[2] = sqlReader.GetString(2);
+                    dRow[3] = sqlReader.GetString(3);
+                    var newRow = dtResult.NewRow();
+                    newRow.ItemArray = dRow;
+                    dtResult.Rows.Add(newRow);
+                }
+            }
+            ProcessedCases = dtResult.Rows.Count;
+            return dtResult;
+        }
+    }   
+}
